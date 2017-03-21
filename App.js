0e4cd78ef9751065f5dd804b19079b06710279dd
nicokant <niccolocantu@gmail.com>
2017-03-21 16:50:12 +0100

    don't insert component directly into App

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Toolbar from './Toolbar';
-import PostsList from './PostsList/';
 
 export default class App extends Component {
 	componentWillMount() {
@@ -12,7 +11,6 @@ export default class App extends Component {
 		return (
 			<View>
 			<Toolbar />
-			<PostsList />
 			</View>
 		);
 	}
