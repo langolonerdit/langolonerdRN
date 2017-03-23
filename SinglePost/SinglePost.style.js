b7c9288b07854168d0f1f8277b29f4677edc5a6d
Aronne Brivio <aroblu94@gmail.com>
2017-03-23 18:17:54 +0100

    Posts style first version - no iframe/remote js

--- SinglePost/SinglePost.style.js
+++ SinglePost/SinglePost.style.js
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  scrollview: {
+    flex: 1,
+    padding: 12,
+  }
 });
 
 export default styles;
--- SinglePost/index.js
+++ SinglePost/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, Image } from 'react-native';
+import { View, WebView, ScrollView, Text, Image } from 'react-native';
 import HTML from 'react-native-fence-html';
+import RemoteComponent from './RemoteComponent';
 import styles from './SinglePost.style';
 
 export default class SinglePost extends Component {
@@ -23,10 +24,62 @@ export default class SinglePost extends Component {
 
   render() {
     const { id, content_full, img, title } = this.props.navigation.state.params
+    const htmlstyle = {
+      i: { 
+        fontStyle: 'italic'
+      },
+      img: { 
+        resizeMode: 'cover',
+        justifyContent: 'center',
+      },
+      code: {
+        padding: 4,
+        color: '#c7254e',
+        backgroundColor: '#f9f2f4',
+        borderRadius: 4,
+      },
+      a: {
+        color: '#89229b',
+        textDecorationLine: 'none',
+      }
+    }
+    const renderers = {
+      img: (htmlAttribs, children, passProps) => {
+        // fence-html-react-native seems to have inverted width and height
+        h = parseInt(htmlAttribs.width)
+        w = parseInt(htmlAttribs.height)
+        return (
+            <Image
+              source={{uri: htmlAttribs.src, width: w, height: h}}
+              style={passProps.htmlStyles.img}
+              {...passProps} />
+        )
+      },
+      // script: (htmlAttribs, children, passProps) => {
+      //   return (
+      //     <View>
+      //       <RemoteComponent
+      //         url={htmlAttribs.src} />
+      //     </View>
+      //   )
+      // },
+      // iframe: (htmlAttribs, children, passProps) => {
+      //   return (
+      //     <View>
+      //       <RemoteComponent
+      //         url={htmlAttribs.src} />
+      //     </View>
+      //   )
+      // }
+    }
     return (  
-      <ScrollView>
-        <Image source={{ uri: `http://www.langolonerd.it/imgs/${img}`}} />
-        <HTML html={ content_full } />
+      <ScrollView style={styles.scrollview}>
+        <Image source={{ uri: `http://www.langolonerd.it/imgs/${img}`}} style={{resizeMode: 'cover'}} />
+        <HTML 
+          html={ content_full }
+          htmlStyles={htmlstyle}
+          onLinkPress={(evt, href) => console.log(href)}
+          renderers={renderers} /> 
       </ScrollView> 
     )
   }
